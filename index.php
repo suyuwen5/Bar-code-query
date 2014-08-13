2702f7f8a241a600aa72ba4ace0bae85919b5f27
suyuwen1 <suyuwen1@126.com>
2014-08-13 17:18:51 +0800

    新添加文件

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>无标题文档</title>
+<link type="text/css" rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+<div id="login">
+	<div id="right">
+    	<div id="logo"><img src="img/aigo.png"></div>
+    </div>
+    <div class="cls"></div>
+</div>
+</body>
+</html>
