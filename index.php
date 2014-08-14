a70b953389a852f69ea9cb942f2101797f6c312a
suyuwen1 <suyuwen1@126.com>
2014-08-14 17:26:59 +0800

    suyuwen

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <div id="login">
 	<div id="left" unselectable="on"><img id="bg" src="img/<?php echo rand(1,3);?>.jpg"></div>
   <div id="right">
-    	<div id="logo"><img src="img/aigo.png"></div>
+    	<div id="logo"><img class="logo_img1" src="img/aigo.png"><div class="cls"></div></div>
         <div id="login_input">
         	<input type="text" maxlength="20">
             <input type="password" maxlength="20">
